ab39213203649727019d2be5f52f0e1041b9d8f6
Artsav23 <107945125+Artsav23@users.noreply.github.com>
2023-06-15 19:23:56 +0300

    Add random music (#1)
    * add music
    
    * add button pause music
    
    * internet connection
    
    * Create gifs
    
    * correct function
    
    * create FlashLight

--- app/src/main/java/com/example/voicehelper/MainActivity.kt
+++ app/src/main/java/com/example/voicehelper/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.voicehelper
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.speech.RecognizerIntent
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.voicehelper.databinding.ActivityMainBinding
 import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var launcher : ActivityResultLauncher<Intent>
     private val wordLibrary = WordLibrary()
     private lateinit var viewModel: ViewModel
+    private lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModel(binding)
         registerForActivityResult()
         viewModel.initTextToSpeech(context = this)
+        handler = viewModel.createHandler(this)
     }
 
     private fun registerForActivityResult() {
@@ -37,37 +45,91 @@ class MainActivity : AppCompatActivity() {
 
     private fun commands(text: String) {
         when {
-            "печенье" in text.lowercase() -> {
+            wordLibrary.find.any { find -> find in text.lowercase() } -> {
+                viewModel.speak("Вот что нашлось по вашему запросу")
+                connectionWithNet(text.lowercase())
+            }
+            wordLibrary.cookies.any { cookies -> cookies in text.lowercase() } -> {
                 binding.imageView.setImageResource(R.drawable.cookie)
                 return
             }
-            wordLibrary.clear.any {it in text.lowercase()} -> {
+            wordLibrary.clear.any {clear -> clear in text.lowercase()} -> {
                 binding.imageView.setImageDrawable(null)
                 return
             }
-            "включи фонарик" in text.lowercase() -> {
-                return
-            }
-            wordLibrary.greeting.any { it == text.lowercase()}  -> {
+            wordLibrary.flashLight.any {flashLight -> wordLibrary.play.any {play ->
+                "$play $flashLight" in text.lowercase()
+            } }  -> viewModel.turnFlashLight(this, true)
+
+            wordLibrary.greeting.any { greeting -> greeting == text.lowercase()}  -> {
                 viewModel.speak("Пока")
                 sleep(500)
                 finish()
                 return
             }
-            wordLibrary.music.any{music-> wordLibrary.play.any{play-> "$play $music" in text.lowercase()}} -> {
+            wordLibrary.music.any{music -> wordLibrary.play.any{play -> "$play $music" in text.lowercase()}} -> {
                 viewModel.playMusic(context = this)
+                binding.pauseMusic.isVisible = true
             }
-            wordLibrary.stop.any { stop -> stop in text.lowercase() || wordLibrary.music.any{music->
+            wordLibrary.stop.any { stop ->  wordLibrary.music.any{music->
                 "$stop $music" in text.lowercase() } } -> {
-                    viewModel.stopMusic()
+                viewModel.stopMusic()
+                binding.pauseMusic.isVisible = false
                 }
-            else -> viewModel.speak("Извините, не знаю такой команды")
+            wordLibrary.stop.any { stop -> stop in text.lowercase() } -> {
+                viewModel.turnFlashLight(this, false)
+                viewModel.stopMusic()
+                binding.pauseMusic.isVisible = false
+            }
 
+            wordLibrary.gif.any{gif -> gif in text.lowercase()} -> {
+                showGif(text.lowercase())
+            }
+            else -> viewModel.speak("Не знаю такой команды")
+
+
+        }
+    }
+
+    private fun showGif(text: String) {
+        val  regex = Regex(wordLibrary.gif.joinToString(separator = "|", transform = Regex::escape))
+        var search = text.replace(regex, "")
+        if (search == "") search = "Gif"
+
+        thread { val url = viewModel.createGif(search)
+            val message = Message.obtain()
+            message.obj = url
+            handler.sendMessage(message)
+        }
+
+    }
+
+    private fun connectionWithNet(text: String) {
+        val regex = Regex(wordLibrary.find.joinToString( separator = "|", transform = Regex::escape))
+        val search = text.replace(regex,"")
+        try {
+            startActivity(viewModel.searchWithInternet(search))
+        } catch (e: Exception) {
+            connectionWithNetCompat(search)
+        }
+    }
+
+    private fun connectionWithNetCompat(text: String) {
+        try {
+            startActivity(viewModel.searchWithInternetCompat(text))
+        }
+        catch (e: Exception) {
+            Toast.makeText(this, "Internet connection error", Toast.LENGTH_SHORT).show()
         }
     }
 
     fun onClickMicrophone(view: View) {
         launcher.launch(viewModel.intentFromMicrophone())
+    }
+
+    fun onClickPause(view: View) {
+        viewModel.stopMusic()
+        binding.pauseMusic.isVisible = false
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/voicehelper/ViewModel.kt
+++ app/src/main/java/com/example/voicehelper/ViewModel.kt
@@ -1,16 +1,29 @@
 package com.example.voicehelper
 
+import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.hardware.camera2.CameraManager
 import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Looper
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
 import com.example.voicehelper.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.Locale
 
-class ViewModel(binding: ActivityMainBinding) {
+class ViewModel(private val binding: ActivityMainBinding) {
     private var mediaPlayer = MediaPlayer()
     private lateinit var textToSpeech: TextToSpeech
+    private val apiKey = "JnoqxlzqBsM6e41fpdhzDnJ1qAPzsuwq"
 
     fun intentFromMicrophone(): Intent {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
@@ -20,8 +33,10 @@ class ViewModel(binding: ActivityMainBinding) {
     }
 
     fun playMusic(context: Context) {
-        mediaPlayer = MediaPlayer.create(context, R.raw.sound0)
+        val musicList = arrayListOf(R.raw.sound0, R.raw.sound1, R.raw.sound2, R.raw.sound3, R.raw.sound4)
+        mediaPlayer = MediaPlayer.create(context, musicList.random())
         mediaPlayer.start()
+        binding.pauseMusic.isVisible = true
     }
 
     fun initTextToSpeech(context: Context) {
@@ -38,5 +53,71 @@ class ViewModel(binding: ActivityMainBinding) {
 
     fun stopMusic() {
         mediaPlayer.stop()
+    }
+
+    fun searchWithInternet(text: String): Intent {
+        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+        intent.putExtra(SearchManager.QUERY, text)
+        return intent
+    }
+    fun createGif(text: String) : String {
+        val jsonString = response(text)
+        val jsonObject = JSONObject(jsonString)
+        val url = jsonObject.getJSONArray("data")
+            .getJSONObject(0)
+            .getJSONObject("images")
+            .getJSONObject("original")
+            .getString("url")
+        return url
+    }
+
+    private fun response(text: String): String {
+        val url = URL("https://api.giphy.com/v1/gifs/search?api_key=$apiKey&q=$text")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        val data: String
+        try {
+            val inputStream = connection.inputStream
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val response = StringBuilder()
+
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+            data = response.toString()
+        } finally {
+            connection.disconnect()
+        }
+        return data
+    }
+
+    fun createHandler(context: Context): android.os.Handler {
+        val handler = android.os.Handler(Looper.getMainLooper()) { message ->
+            val data = message.obj as String
+            try {
+
+                Glide.with(context).load(data).into(binding.imageView)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    context,
+                    "По вашему запросу ничего не было найдено",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            true
+        }
+        return handler
+    }
+
+    fun searchWithInternetCompat(text: String): Intent {
+        val uri = Uri.parse("http://www.google.com/#q=$text")
+        return Intent(Intent.ACTION_VIEW, uri)
+    }
+
+    fun turnFlashLight(context: Context, status: Boolean) {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        cameraManager.setTorchMode(cameraId, status)
     }
 }
